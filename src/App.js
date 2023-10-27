26de090b58753482da987108cf8dcc2e5af91d07
Ravi Shankar <chiragj97.cj@gmail.com>
2023-10-28 00:22:26 +0530

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import Carousel from './components/Carousel';
-import MainPage from './components/MainPage';
 import ResponsiveAppBar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Booking from './components/Booking';
+import Home from './components/Home';
 function App() {
   return (
     <Router>
-      <div className='App'>
-        <ResponsiveAppBar />
-        {/* <Carousel />
-        <MainPage /> */}
-        <Booking />
-      </div>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/booking' element={<Booking />} />
+      </Routes>
     </Router>
   );
 }
