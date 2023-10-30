a2fd494cadb8f7bb1455f34447f387b77ca89866
Ravi Shankar <chiragj97.cj@gmail.com>
2023-10-30 19:04:05 +0530

    added with json servers

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Button from '@mui/material/Button';
@@ -7,7 +7,23 @@ import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import Order from './Order';
+import { getMenu } from '../api/menu'
+
 export default function MainPage() {
+  const [menu, setMenu] = useState([])
+  const [category, setCategory] = useState({})
+
+  useEffect(() => {
+    getMenu().then((menu) => {
+      setMenu(menu)
+      setCategory(menu[0])
+    })
+  }, [])
+
+  const handleClick = (category) => {
+    setCategory(category)
+  }
+
   return (
     <Box sx={{ width: '100%', bgcolor: 'background.paper', p: 4 }}>
       <Box sx={{ my: 3, mx: 2 }}>
@@ -34,17 +50,20 @@ export default function MainPage() {
           Popular cuisines
         </Typography>
         <Stack direction='row' spacing={1}>
-          <Chip label='Pizza' />
+          {menu.map((menu, i) => (
+            <Chip onClick={() => handleClick(menu)} key={i} label={menu.category} />
+          ))}
+          {/* <Chip label='Pizza' />
           <Chip color='primary' label='Burger' />
           <Chip label='Rolls' />
           <Chip label='Salad' />
           <Chip label='Noodles' />
           <Chip label='Lasagna' />
-          <Chip label='Pasta' />
+          <Chip label='Pasta' /> */}
         </Stack>
       </Box>
       <br />
-      <Order />
+      <Order category={category} />
       <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
         <Button>Add to cart</Button>
       </Box>
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,40 +1,45 @@
 import image1 from '../assets/image1.jpeg';
-const Order = () => {
+const Order = (props) => {
+  console.log(props)
   return (
-    <div className='row p-4'>
-      <div className='col'>
-        <h3>Noodles</h3>
-        <h6>
-          A cooked egg-and-flour paste prominent in European and Asian cuisine,
-          generally distinguished from pasta by its elongated ribbonlike form.
-          Noodles are commonly used to add body and flavour to broth soups.
-        </h6>
-        <p>$5.46</p>
-      </div>
-      <div className='col d-flex flex-row-reverse'>
-        <div>
-          <img
-            src={image1}
-            style={{
-              height: '120px',
-              objectFit: 'cover',
-              borderRadius: '10px',
-            }}
-            alt='image1'
-          />
-          <div className='border d-flex justify-content-between p-1'>
-            <div className='p-1' style={{ fontSize: 18, fontWeight: 'bold' }}>
-              -
-            </div>
-            <div style={{ fontSize: 18 }}>0</div>
-            <div className='p-1' style={{ fontSize: 18, fontWeight: 'bold' }}>
-              +
+    <>
+      <h2>{props.category.category}</h2>
+      {props?.category?.items?.map((item) => (
+        <div className='row p-4'>
+          <div className='col'>
+            <h3>{item?.name}</h3>
+            <h6>
+              {item?.description}
+            </h6>
+            <p>${item?.price}</p>
+          </div>
+          <div className='col d-flex flex-row-reverse'>
+            <div>
+              <img
+                src={item?.image}
+                style={{
+                  height: '120px',
+                  objectFit: 'cover',
+                  borderRadius: '10px',
+                }}
+                alt='image1'
+              />
+              <div className='border d-flex justify-content-between p-1'>
+                <div className='p-1' style={{ fontSize: 18, fontWeight: 'bold' }}>
+                  -
+                </div>
+                <div style={{ fontSize: 18 }}>0</div>
+                <div className='p-1' style={{ fontSize: 18, fontWeight: 'bold' }}>
+                  +
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
+      ))}
+    </>
+
+  )
+}
 
 export default Order;
